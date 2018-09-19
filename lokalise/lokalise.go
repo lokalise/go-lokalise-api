de6bdabfc9fac948556d96fcd8b7b2d544c6bcaa
Crevil <bjoern.soerensen@gmail.com>
2018-09-19 12:53:15 +0200

    Retry handling in base client
    #4

--- lokalise/lokalise.go
+++ lokalise/lokalise.go
@@ -2,7 +2,9 @@
 package lokalise
 
 import (
+	"errors"
 	"io"
+	"net/http"
 	"time"
 
 	"github.com/go-resty/resty"
@@ -20,12 +22,45 @@ type client struct {
 type option func(*client) error
 
 func newClient(apiToken string, options ...option) (*client, error) {
-	c := client{}
+	c := client{
+		retryCount: 3,
+	}
 	for _, o := range options {
 		err := o(&c)
 		if err != nil {
 			return nil, err
 		}
 	}
+	c.client = resty.New().
+		SetRetryCount(c.retryCount).
+		AddRetryCondition(requestRetryCondition())
+
 	return &c, nil
+}
+
+// withRetryCount returns a client option setting the retry count of outgoing requests.
+// if count is zero retries are disabled.
+func withRetryCount(count int) option {
+	return func(c *client) error {
+		if count < 0 {
+			return errors.New("lokalise: retry count must be positive")
+		}
+		c.retryCount = count
+		return nil
+	}
+}
+
+// requestRetryCondition indicates a retry if the HTTP status code of the response
+// is >= 500.
+// failing requests due to network conditions, eg. "no such host", are handled by resty internally
+func requestRetryCondition() resty.RetryConditionFunc {
+	return func(res *resty.Response) (bool, error) {
+		if res == nil {
+			return true, nil
+		}
+		if res.StatusCode() >= http.StatusInternalServerError {
+			return true, nil
+		}
+		return false, nil
+	}
 }
---
+++ lokalise/lokalise_test.go
@@ -0,0 +1,222 @@
+package lokalise
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/go-resty/resty"
+)
+
+func TestClient(t *testing.T) {
+	type input struct {
+		retryCount  int
+		okAtAttempt int
+	}
+	type output struct {
+		attempts   int
+		statusCode int
+		err        error
+	}
+	tt := []struct {
+		name   string
+		input  input
+		output output
+	}{
+		{
+			name: "internal error on all attempts",
+			input: input{
+				retryCount:  2,
+				okAtAttempt: 5,
+			},
+			output: output{
+				attempts:   2,
+				statusCode: http.StatusInternalServerError,
+				err:        nil,
+			},
+		},
+		{
+			name: "ok at last attempts",
+			input: input{
+				retryCount:  2,
+				okAtAttempt: 2,
+			},
+			output: output{
+				attempts:   2,
+				statusCode: http.StatusOK,
+				err:        nil,
+			},
+		},
+		{
+			name: "zero retries",
+			input: input{
+				retryCount:  0,
+				okAtAttempt: 2,
+			},
+			output: output{
+				attempts:   1,
+				statusCode: http.StatusInternalServerError,
+				err:        nil,
+			},
+		},
+		{
+			name: "default to three retries",
+			input: input{
+				retryCount:  -1, // disables the retry option in test
+				okAtAttempt: 3,
+			},
+			output: output{
+				attempts:   3,
+				statusCode: http.StatusOK,
+				err:        nil,
+			},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			attempts := 0
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				attempts++
+				if attempts >= tc.input.okAtAttempt {
+					rw.WriteHeader(http.StatusOK)
+					return
+				}
+				rw.WriteHeader(http.StatusInternalServerError)
+			}))
+			var opts []option
+			if tc.input.retryCount >= 0 {
+				opts = append(opts, withRetryCount(tc.input.retryCount))
+			}
+			c, err := newClient("token", opts...)
+			if err != nil {
+				t.Fatalf("client instantiation error: %v", err)
+			}
+			response, err := c.client.R().Get(server.URL)
+			if tc.output.err != nil {
+				if err == nil {
+					t.Fatalf("expected error %s but got nil", tc.output.err)
+				}
+				if tc.output.err.Error() != err.Error() {
+					t.Fatalf("wrong error: expected '%s': got '%s'", tc.output.err.Error(), err.Error())
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("expected no error but got '%s'", err.Error())
+			}
+			if response == nil {
+				t.Fatal("expected a response but got nil")
+			}
+			if response.StatusCode() != tc.output.statusCode {
+				t.Fatalf("wrong response status code: expected %d: got %d", tc.output.statusCode, response.StatusCode())
+			}
+			if tc.output.attempts != attempts {
+				t.Fatalf("expected %d request attempts: got %d", tc.output.attempts, attempts)
+			}
+		})
+	}
+}
+
+func TestClient_contextCancelation(t *testing.T) {
+	attempts := 0
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		attempts++
+		time.Sleep(10 * time.Millisecond)
+		rw.WriteHeader(http.StatusOK)
+	}))
+	c, err := newClient("token")
+	if err != nil {
+		t.Fatalf("client instantiation error: %v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	_, err = c.client.R().SetContext(ctx).Get(server.URL)
+	if err == nil {
+		t.Fatal("expected an error but got nil")
+	}
+	if ctx.Err() == nil {
+		t.Fatal("expected context to be canceled")
+	}
+	if attempts != 0 {
+		t.Fatalf("expected context cancelation not to be retried but got %d attempts", attempts)
+	}
+}
+
+func TestClient_negativeRetries(t *testing.T) {
+	_, err := newClient("token", withRetryCount(-1))
+	if err == nil {
+		t.Fatal("expected an error but got nil")
+	}
+}
+
+func TestRequestRetryCondition(t *testing.T) {
+	tt := []struct {
+		name        string
+		response    *resty.Response
+		shouldRetry bool
+		err         error
+	}{
+		{
+			name:        "nil response",
+			response:    nil,
+			shouldRetry: true,
+			err:         nil,
+		},
+		{
+			name: "status code 200",
+			response: &resty.Response{
+				Request: &resty.Request{},
+				RawResponse: &http.Response{
+					StatusCode: 200,
+				},
+			},
+			shouldRetry: false,
+			err:         nil,
+		},
+		{
+			name: "status code 499",
+			response: &resty.Response{
+				Request: &resty.Request{},
+				RawResponse: &http.Response{
+					StatusCode: 499,
+				},
+			},
+			shouldRetry: false,
+			err:         nil,
+		},
+		{
+			name: "status code 500",
+			response: &resty.Response{
+				Request: &resty.Request{},
+				RawResponse: &http.Response{
+					StatusCode: 500,
+				},
+			},
+			shouldRetry: true,
+			err:         nil,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			retry, err := requestRetryCondition()(tc.response)
+			if tc.err != nil {
+				if err == nil {
+					t.Fatalf("expected error %s but got nil", tc.err)
+				}
+				if tc.err.Error() != err.Error() {
+					t.Fatalf("wrong error: expected '%s': got '%s'", tc.err.Error(), err.Error())
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("expected no error but got '%s'", err.Error())
+			}
+			if tc.shouldRetry != retry {
+				t.Fatalf("expected retry to be %t but got %t", tc.shouldRetry, retry)
+			}
+		})
+	}
+}
