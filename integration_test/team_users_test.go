06e0230746af2c6e9da141f32124c92ddedfcf6a
Crevil <bjoern.soerensen@gmail.com>
2018-10-15 14:25:36 +0200

    Move projects integration test into own file

--- integration_test/team_users_test.go
+++ integration_test/team_users_test.go
@@ -102,29 +102,3 @@ func TestGetTeams(t *testing.T) {
 	t.Logf("teams %+v", resp.Teams)
 	t.Logf("paged %+v", resp.Paged)
 }
-
-func TestGetProjects(t *testing.T) {
-	flag.Parse()
-	if token == "" {
-		t.Errorf("set token flag to run integration tests")
-		return
-	}
-	client, err := lokalise.NewClient(token, lokalise.WithRetryCount(5))
-	if err != nil {
-		t.Fatalf("client instantiation: %v", err)
-	}
-
-	resp, err := client.Projects.List(context.Background(), lokalise.ProjectsOptions{
-		TeamID: 170090,
-		PageOptions: lokalise.PageOptions{
-			Limit: 0,
-			Page:  1,
-		},
-	})
-
-	if err != nil {
-		t.Fatalf("request err: %v", err)
-	}
-	t.Logf("teams %+v", resp.Projects)
-	t.Logf("paged %+v", resp.Paged)
-}
